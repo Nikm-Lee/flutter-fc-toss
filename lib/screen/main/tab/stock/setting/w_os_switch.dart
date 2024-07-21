fb5a88312c03d8f59ac3199ed6d60d2101d1b76c
Nikm-Lee <kmdiabolos@naver.com>
2024-07-21 18:34:09 +0900

    Part3. Ch02 17-18

---
+++ lib/screen/main/tab/stock/setting/w_os_switch.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class OsSwitch extends StatelessWidget {
+  final bool isOn;
+  final ValueChanged<bool> onChanged;
+
+  const OsSwitch({super.key, required this.isOn, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoSwitch(value: isOn, onChanged: onChanged)
+        : Switch(value: isOn, onChanged: onChanged);
+  }
+}
