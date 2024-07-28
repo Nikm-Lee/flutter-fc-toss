8b428edf39132791fcc18f00abfa39ddcf118473
Nikm-Lee <kmdiabolos@naver.com>
2024-07-28 14:05:11 +0900

    part3-ch3-04 애니메이션은 지금 필요없어보여서 여기까지하고 ch04로 넘어검

--- lib/screen/main/tab/stock/search/w_popular_stock_item.dart
+++ lib/screen/main/tab/stock/search/w_popular_stock_item.dart
@@ -1,4 +1,6 @@
+import 'package:animations/animations.dart';
 import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/screen/main/tab/stock/search/s_stock_detail.dart';
 import 'package:fast_app_base/screen/main/tab/stock/vo_popular_stock.dart';
 import 'package:flutter/material.dart';
 
@@ -10,23 +12,25 @@ class PopularStockItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: context.backgroundColor,
-      padding: const EdgeInsets.symmetric(vertical: 10),
-      child: Row(
-        children: [
-          SizedBox(
-            width: 40,
-            child: rank.text.make(),
-          ),
-          width30,
-          stock.name.text.make(),
-          emptyExpanded,
-          stock.todayPercentageString.text
-              .color(stock.getPriceColor(context))
-              .make()
-        ],
-      ),
-    );
+    return OpenContainer<bool>(
+        openColor: context.backgroundColor,
+        closedColor: context.backgroundColor,
+        openBuilder: (BuildContext context, VoidCallback _) {
+          return StockDetailScreen(stock.name);
+        },
+        closedBuilder: (BuildContext context, VoidCallback action) {
+          return Row(
+            children: [
+              rank.text.bold.white.size(16).make(),
+              width20,
+              stock.name.text.bold.white.size(16).make(),
+              emptyExpanded,
+              stock.todayPercentageString.text
+                  .color(stock.getPriceColor(context))
+                  .size(16)
+                  .make()
+            ],
+          ).pSymmetric(h: 20, v: 20);
+        });
   }
 }
